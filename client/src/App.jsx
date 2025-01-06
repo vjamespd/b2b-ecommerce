f271edd4c541656ca38f1f657cbea5c880639653
vjamespd <dvinardjames@gmail.com>
2025-01-07 02:10:34 +0800

    CheckAuth based on user Roles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,26 +13,44 @@ import ShoppingHome from "./pages/shopping-view/home";
 import ShoppingListing from "./pages/shopping-view/listing";
 import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingAccount from "./pages/shopping-view/account";
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
 
 function App() {
+
+  const isAuthenticated = true;
+  const user = { role: "user" }
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
 
-
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+          }>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
 
-        <Route path="/shop" element={<ShoppingLayout/>}>
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout/>
+          </CheckAuth>
+        }>
           <Route path="home" element={<ShoppingHome/>}/>
           <Route path="listing" element={<ShoppingListing/>}/>
           <Route path="checkout" element={<ShoppingCheckout/>}/>
@@ -41,6 +59,7 @@ function App() {
         </Route>
 
         <Route path="*" element={<NotFound/>} />
+        <Route path="/unauth-page" element={<UnauthPage/>}/>
       </Routes>
     </div>
   );
